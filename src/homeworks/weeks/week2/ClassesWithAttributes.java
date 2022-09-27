41c5703b25fce0a883efac7ff61b4ed3c841bd53
samiayazz <samiayazdv@gmail.com>
2022-09-27 21:59:35 +0300

    feat: 2nd week, 'Getter Setter | Encapsulation' homework

--- src/homeworks/weeks/week2/ClassesWithAttributes.java
+++ src/homeworks/weeks/week2/ClassesWithAttributes.java
@@ -5,29 +5,84 @@ import homeworks.weeks.Homework;
 public class ClassesWithAttributes implements Homework {
     public void executeHomework() {
         Product product = new Product();
-        product.id = 1;
-        product.name = "Laptop";
-        product.description = "Asus Laptop";
-        product.price = 13000;
-        product.stockAmount = 3;
+        product.setId(1);
+        product.setName("Macbook Pro 16");
+        product.setDescription("2022 16\" M1 Macbook Pro");
+        product.setPrice(49999);
+        product.setStockAmount(10);
+        product.setColor("Space Gray");
 
         ProductManager productManager = new ProductManager();
         productManager.add(product);
     }
 
-    private class Product {
-        //attribute | field
-        int id;
-        String name;
-        String description;
-        double price;
-        int stockAmount;
-    }
-
     private class ProductManager {
         public void add(Product product) {
             //JDBC
-            System.out.println("Ürün eklendi: " + product.name);
+            System.out.println("Ürün eklendi: " + product.getCode());
+        }
+    }
+
+    private class Product {
+        //attribute | field
+        private int id;
+        private String name;
+        private String description;
+        private double price;
+        private int stockAmount;
+        private String color;
+        private String code;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+        public int getStockAmount() {
+            return stockAmount;
+        }
+
+        public void setStockAmount(int stockAmount) {
+            this.stockAmount = stockAmount;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public String getCode() {
+            return name.substring(0, 1) + id;
         }
     }
 }
