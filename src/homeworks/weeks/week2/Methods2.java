6a21185666a67c5ebe1c48c2c6d5c8739d025058
samiayazz <samiayazdv@gmail.com>
2022-09-27 19:58:05 +0300

    feat: 2nd week, 'Variable Arguments' homework

--- src/homeworks/weeks/week2/Methods2.java
+++ src/homeworks/weeks/week2/Methods2.java
@@ -9,6 +9,8 @@ public class Methods2 implements Homework {
         System.out.println(yeniMesaj);
         int sayi = topla(15, 7);
         System.out.println(sayi);
+        int toplam = topla2(2,3,4,5,6,10);
+        System.out.println(toplam);
     }
 
     public static void ekle() {
@@ -25,6 +27,15 @@ public class Methods2 implements Homework {
 
     public static int topla(int sayi1, int sayi2) {
         return sayi1 + sayi2;
+    }
+
+    public static int topla2(int... sayilar) {
+        int toplam = 0;
+        for (int sayi : sayilar) {
+            toplam += sayi;
+        }
+
+        return toplam;
     }
 
     public static String sehirVer() {
