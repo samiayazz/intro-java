11d8814bf28b06b24021ded55bde2de99f02f9d4
samiayazz <samiayazdv@gmail.com>
2022-09-27 20:12:49 +0300

    feat: 2nd week, 'Learning: Value and Reference Types' homework

--- src/homeworks/weeks/week2/Classes.java
+++ src/homeworks/weeks/week2/Classes.java
@@ -10,6 +10,20 @@ public class Classes implements Homework {
         customerManager.add();
         customerManager.remove();
         customerManager.update();
+
+        //value
+        int sayi1 = 10;
+        int sayi2 = 20;
+        sayi2 = sayi1;
+        sayi1 = 30;
+        System.out.println(sayi2);
+
+        //reference
+        int[] sayilar1 = {1, 2, 3};
+        int[] sayilar2 = {4, 5, 6};
+        sayilar2 = sayilar1;
+        sayilar1[0] = 10;
+        System.out.println(sayilar2[0]);
     }
 
     private class CustomerManager {
